80e9ede8664ad0f69a95073dcad41a8f250376d7
Zhenya Telyukov <telyukov@gmail.com>
2024-04-19 13:48:49 +0300

    locales update

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Wort
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:de, :en, :ru]
   end
 end
